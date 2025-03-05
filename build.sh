dcf902913757ac5680fec2129ca25d0d56bdcf90
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-03-05 18:19:06 +0000

    build root dir

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ if [ ! -d opencv_contrib ]; then
 fi
 if [ ! -d opencv ]; then
     git clone --branch 4.6.0 --depth=1 https://github.com/opencv/opencv.git
-    cd opencv/platforms/linux/ && mkdir build && cd build && \
+    cd opencv/platforms/linux/ && mkdir -p build && cd build && \
     cmake -DCMAKE_TOOLCHAIN_FILE=../aarch64-gnu.toolchain.cmake \
         -DCMAKE_BUILD_TYPE=Release -D BUILD_opencv_python=OFF -D BUILD_opencv_python2=OFF -D BUILD_opencv_python3=OFF \
         -DOPENCV_EXTRA_MODULES_PATH=/root/opencv_contrib/modules \
@@ -34,7 +34,7 @@ fi
 if [ ! -d arrow ]; then
     git clone --branch apache-arrow-19.0.1 --depth=1 https://github.com/apache/arrow.git
     cd arrow/cpp && \
-    mkdir "build" && cd build && \
+    mkdir -p build && cd build && \
     cmake -DCMAKE_TOOLCHAIN_FILE=$DIRPATH/toolchain.cmake \
         -DCMAKE_BUILD_TYPE=Release -DARROW_BUILD_STATIC=ON -DARROW_DEPENDENCY_SOURCE=BUNDLED \
         -DARROW_ENABLE_THREADING=OFF -DARROW_FLIGHT=OFF -DARROW_JEMALLOC=OFF -DARROW_MIMALLOC=OFF -DARROW_BUILD_SHARED=OFF \
@@ -49,10 +49,17 @@ if [! -d rerun ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     source $HOME/.cargo/env
     cd rerun && \
-    mkdir "build" && cd build && \
+    mkdir -p build && cd build && \
     cmake -DCMAKE_TOOLCHAIN_FILE=$DIRPATH/toolchain.cmake \
         -DCMAKE_BUILD_TYPE=Release -DRERUN_DOWNLOAD_AND_BUILD_ARROW=OFF \
         -DCMAKE_INSTALL_PREFIX=/usr/local .. && \
     make -j $(nproc) && make install
     cd ../..
 fi
+
+mkdir -p build && cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=$DIRPATH/toolchain.cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local .. && \
+make -j $(nproc) && make install
+cd ..
