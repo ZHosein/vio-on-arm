20be7b03d11aee4ab879f768611fc07ab40955c9
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-03-07 12:27:19 +0000

    revert to non-static linking

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,7 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
-set(CMAKE_EXE_LINKER_FLAGS "-static -static-libgcc -static-libstdc++")
-set(CMAKE_FIND_LIBRARY_SUFFIXES ".a") # Prefer static libraries
 
 set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc-8)
 set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++-8)
