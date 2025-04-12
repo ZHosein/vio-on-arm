4e85fb95459947477f00cc8009c639203b82e984
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-04-12 07:41:59 -0400

    `arm` keyword used instead of `aarch` for boost and gtsam

--- build.sh
+++ build.sh
@@ -74,10 +74,10 @@ if [ ! -d boost_1_71_0 ]; then
     wget https://archives.boost.io/release/1.71.0/source/boost_1_71_0.tar.gz
     tar -xzf boost_1_71_0.tar.gz
     cd boost_1_71_0
-    echo "using gcc : aarch64 : aarch64-linux-gnu-g++ ;" > user-config.jam
+    echo "using gcc : arm : aarch64-linux-gnu-g++ ;" > user-config.jam
     ./bootstrap.sh
-    ./b2 --user-config=./user-config.jam toolset=gcc-aarch64 \
-        --prefix=/usr/local/boost-aarch64 --build-dir=build-aarch64 \
+    ./b2 --user-config=./user-config.jam toolset=gcc-arm \
+        --prefix=/usr/local/boost-arm --build-dir=build-arm \
         --with-system --with-filesystem --with-thread --with-date_time --with-chrono --with-regex \
         target-os=linux link=static runtime-link=static threading=multi \
         architecture=arm address-model=64 install
@@ -97,6 +97,9 @@ if [ ! -d gtsam ]; then
         -DGTSAM_BUILD_TESTS=OFF \
         -DGTSAM_BUILD_EXAMPLES=OFF \
         -DCMAKE_INSTALL_PREFIX=/usr/local \
+        -DBOOST_ROOT=/usr/local/boost-arm \
+        -DBoost_NO_SYSTEM_PATHS=ON \
+        -DBoost_USE_STATIC_LIBS=ON \
         .. && \
     make -j $(nproc) && make install
     cd ../..
