8d259be948fac56476ab1ae152e126b3c2591f0e
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-04-16 00:05:00 +0000

    modularity

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include "slamProto3.cpp"
+#include "baby_vSLAM.cpp"
 
-int main()
-{
-    std::cout << "Starting iSAM SLAM" << std::endl;
-    prototype3::slam();
+int main(int argc, char **argv){
+    std::cout << "Starting iSAM SLAM\n";
+    baby_vSLAM::process_folder_cli(argc, argv);
     std::cout << "Slam Algorithm run Complete\n";
     return 0;
 }
