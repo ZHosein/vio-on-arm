e0063ee119b1436bbe6f87b337a87591dc4f43c9
ZHosein <72307057+ZHosein@users.noreply.github.com>
2025-04-23 12:57:43 -0400

    Revert "Cross-compilation toolchain of vSLAM prototypes"

--- tiny_arm_slam.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "tiny_arm_slam.hpp"
-
-int main(int argc, char **argv){
-    std::cout << "Starting Tiny ARM SLAM\n";
-    tiny_arm_slam::process_folder_cli(argc, argv);
-    std::cout << "Tiny ARM SLAM run Complete\n";
-    return 0;
-}
